14519296cee382bdedb503a42b1bb413919d5dbd
Will Houghton <contact@willhoughton.me>
2019-09-06 09:54:07 +1200

    pushing to merge

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
 
-import { getFruits } from '../apiClient'
+import Footer from './Footer'
+import Main from './Main'
+import Nav from './Nav'
 
-class App extends React.Component {
-  state = {
-    fruits: []
-  }
-
-  componentDidMount () {
-    getFruits()
-      .then(fruits => {
-        this.setState({fruits})
-      })
-  }
-
-  render () {
-    return (
-      <div className='app'>
-        <h1>Fullstack Boilerplate</h1>
-        <ul>
-          {this.state.fruits.map(fruit => (
-            <li key={fruit}>{fruit}</li>
-          ))}
-        </ul>
-      </div>
-    )
-  }
+const App = () => {
+  <div>
+    <Nav />
+    <Main />
+    <Footer />
+  </div>
 }
 
-export default App
+export default App 
