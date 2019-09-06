4e1aba72d5836343cd3a16f23ad435da9d7b7540
rosedhlakama <rosejembe@live.com>
2019-09-06 12:48:03 +1200

    got hold of key event on form

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -1,25 +1,42 @@
 import React from 'react'
 
-class Popup extends React.Component {  
-    render() {  
-  return (  
-  <div className='popup'>  
-    <div className='popupinner'>  
-    <h1>{this.props.text}</h1>  
-    <form>
+class Popup extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {input: ''}
+  }
+
+
+  messWithWords = (e) => {
+    let keyPressed = e.key
+    if((Math.random() * 10) > 8) { 
+    } else {
+      this.setState({input: this.state.input + keyPressed})
+    }
+    console.log(keyPressed, e.target.value)
+  }
+
+  render() {
+    return (
+      <div className='popup'>
+        <div className='popupinner'>
+          <h1>{this.props.text}</h1>
+          <form>
             <label>
-                Name:
-                <input type="text" name="name" />
+              Name:
+                <input type="text" name="name" onKeyPress={this.messWithWords} value={this.state.input} />
             </label>
-                <input type="submit" value="Submit" />
-                <button onClick={this.props.closePopup}>close me</button>  
-        </form>  
-    </div>
-    
-  </div>  
- 
-  );  
-  }  
-  }  
-  
-  export default Popup
+            <input type="submit" value="Submit" />
+            <button onClick={this.props.closePopup}>close me</button>
+          </form>
+        </div>
+
+      </div>
+
+    );
+  }
+}
+
+export default Popup
